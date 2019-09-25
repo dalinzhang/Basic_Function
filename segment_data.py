a4f9aabfc1ec2423719962b5debc2ab2631da92f
Dalin Zhang <zhangdalin90@gmail.com>
2019-09-25 14:57:15 +1000

    Update segment_data.py

--- segment_data.py
+++ segment_data.py
@@ -9,9 +9,9 @@ def windows(data, size, step):
 
 
 def segment_signal_without_transition(data, window_size, step):
-'''
-data has shape of [n_timelength]
-'''
+    '''
+    data has shape of [n_timelength]
+    '''
     segments = []
     for (start, end) in windows(data, window_size, step):
         if(len(data[start:end]) == window_size):
@@ -20,9 +20,9 @@ data has shape of [n_timelength]
 
 
 def segment_dataset(X, window_size, step):
-'''
-X has shape of [n_sample, n_timelength]
-'''
+    '''
+    X has shape of [n_sample, n_timelength]
+    '''
     win_x = []
     for i in range(X.shape[0]):
         win_x = win_x + [segment_signal_without_transition(X[i], window_size, step)]
